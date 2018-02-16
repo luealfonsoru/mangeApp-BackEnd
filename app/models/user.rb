2fcb265c5285f6b43ff932b7f4f8054ad98276de
Luis Enrique Alfonso Rubio <luis.alfonso1>
2018-02-16 13:10:54 -0500

    Model bug fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_many :user_proyects, dependent: :destroy
-  belongs_to :user
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
